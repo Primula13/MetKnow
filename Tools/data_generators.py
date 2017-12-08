0afb05bf7bc2d394e0e42809e220e7c71bf2aec7
Primula13 <kornutaa@gmail.com>
2017-12-08 10:46:03 +0200

    Add files via upload

--- Tools/data_generators.py
+++ Tools/data_generators.py
@@ -1,28 +1,37 @@
 import requests
+import json
 from Main.frames import models
 from mimesis import Generic
 generic = Generic('en')
 
 
 def create_groups():
-    auth = {"Authorization": "Bearer " + models.user1.token}
-    payload = {"name": generic.text.word(), "description": generic.text.text(quantity=1)}
-    requests.put(url="http://metknow.dev.cleveroad.com/api/Group/CreateGroup", headers=auth, data=payload)
+    auth = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + models.user1.token
+        }
+    for _ in range(30):
+        payload = {"name": generic.text.word(), "description": generic.text.word()}
+        requests.put(url="http://metknow.dev.cleveroad.com/api/Group/CreateGroup", headers=auth,
+                         data=json.dumps(payload))
 
 
 def create_members_in_group():
-    auth = {"Authorization": "Bearer " + models.user1.token}
+    auth = {
+        "Content-Type": "application/json",
+        "Authorization": models.user1.token
+        }
     for _ in range(60):
         payload = {
             "firstName": generic.personal.name(),
             "lastName": generic.personal.surname(),
             "email": generic.personal.email(),
-            "phone": generic.personal.telephone(),
+            "phone": "54755757858",
             "gender": generic.personal.gender(),
             "city": generic.address.city(),
-            "company": generic.business.company(),
-            "whereWeMet": generic.text.text(quantity=1),
+            "company": generic.business.company()
         }
-        requests.put(url="http://metknow.dev.cleveroad.com/api/Member/30/CreateMember", headers=auth,
-                     data=payload)
+        r = requests.put(url="http://metknow.dev.cleveroad.com/api/Member/76/CreateMember", headers=auth,
+                         data=json.dumps(payload))
+        return r.status_code, r.json()
 
